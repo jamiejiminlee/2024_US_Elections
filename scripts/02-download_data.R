3ea34c97a76ef2328ed47728b6cb6b49f0f74848
Jamie Lee <jamielee@Jamies-MacBook-Pro.local>
2024-10-19 15:51:03 -0400

    committing 01-download_data script

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -9,18 +9,14 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+raw_data <- read_csv("/Users/jamielee/Downloads/president_polls.csv")
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(raw_data, "data/01-raw_data/raw_data.csv")
