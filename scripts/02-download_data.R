c63e7852c987db98e89531ed6a1c5e3018581c7b
Jamie Lee <jamielee@Jamies-MacBook-Pro.local>
2024-10-19 17:22:22 -0400

    committing 03-clean_data script and cleaned dataset

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -13,10 +13,10 @@ library(tidyverse)
 
 
 #### Download data ####
-raw_data <- read_csv("/Users/jamielee/Downloads/president_polls.csv")
+raw_poll_data <- read_csv("/Users/jamielee/Downloads/president_polls.csv")
 
 
 #### Save data ####
 # [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(raw_data, "data/01-raw_data/raw_data.csv")
+
+write_csv(raw_poll_data, "data/01-raw_data/raw_data.csv")
